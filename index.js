47154b7c235d53a6cb68970c5782cc3cc75e0c27
Petro Shevchyshen <shevchyshen@gmail.com>
2025-03-21 22:51:09 +0200

    changed log message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Word 11")
+  res.send("Hello Word")
 });
 
 const PORT = process.env.PORT || 3000;
